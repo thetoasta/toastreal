bdf1769049bbaf9d8531809cbc3b03796a50c2b3
The Toasta <nolanp678@gmail.com>
2025-01-29 21:14:03 +0000

    a

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAIi3_sc81JogKi_UjMGYqUMvsf4WBDJPc",
+    authDomain: "toastrealxyz.firebaseapp.com",
+    projectId: "toastrealxyz",
+    storageBucket: "toastrealxyz.appspot.com",
+    messagingSenderId: "774884910313",
+    appId: "1:774884910313:web:e11002887adf1055acfc78",
+    measurementId: "G-3H38LZB2BX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
